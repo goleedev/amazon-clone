c631d176e39b4762af9166e3ac7318f0afb2fc45
GO Lee <golee.dev@gmail.com>
2020-06-12 23:46:54 +0900

    Add to cart function with redux

--- amazon-clone/src/Screens/ProductScreen.js
+++ amazon-clone/src/Screens/ProductScreen.js
@@ -1,61 +1,90 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  console.log(props.match.params.id);
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
 
-  const product = data.products.find(x => x._id === props.match.params.id);
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+      //
+    };
+  }, []);
+
+  const handleAddCart = () => {
+    props.history.push("/cart/" + props.match.params.id + "?qty=" + qty);
+  };
 
   return (
     <div>
       <div className="back-to-result">
-        <Link to="/">Back to results</Link>
+        <Link to="/"> &#60; Back to results</Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt="product"></img>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="product"></img>
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </li>
+              <li>
+                Price: <b>${product.price}</b>
+              </li>
+              <li>
+                Description: <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>Price: ${product.price}</li>
+              <li>
+                Status: {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+              </li>
+              <li>
+                Qty:
+                <select
+                  value={qty}
+                  onChange={e => {
+                    setQty(e.target.value);
+                  }}
+                >
+                  {[...Array(product.countInStock).keys()].map(x => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </li>
+              <li>
+                {product.countInStock > 0 && (
+                  <button
+                    onClick={handleAddCart}
+                    className="cart-button primary"
+                  >
+                    Add to Cart
+                  </button>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </li>
-            <li>
-              Price: <b>${product.price}</b>
-            </li>
-            <li>
-              Description: <div>{product.description}</div>
-            </li>
-          </ul>
-        </div>
-        <div className="details-action">
-          <ul>
-            <li>Price: ${product.price}</li>
-            <li>Status: {product.status}</li>
-            <li>
-              Qty:{" "}
-              <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-              </select>
-            </li>
-            <li>
-              <button className="cart-button primary">Add to Cart</button>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
